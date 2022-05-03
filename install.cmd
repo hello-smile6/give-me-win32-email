d0d27fb133d4945218d8acac9573fe35262bba06
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-05-02 19:57:09 -0700

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,3 +1,4 @@
+@echo OFF
 curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.appx --output "%TEMP%\UI.Xaml.appx"
 powershell -c "Add-AppxPackage %TEMP%\UI.Xaml.appx"
 curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.appx --output "%TEMP%\vclibs.appx"
