6ae7402e460dd19e7875837f4306ad9e79f094d2
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-05-02 20:03:39 -0700

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,7 +1,8 @@
 @echo OFF
 curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.appx --output "%TEMP%\UI.Xaml.appx"
 powershell -c "Add-AppxPackage %TEMP%\UI.Xaml.appx"
-curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.appx --output "%TEMP%\vclibs.appx"
+rem curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/Microsoft.VCLibs.140.00_14.0.30704.0_x64__8wekyb3d8bbwe.appx --output "%TEMP%\vclibs.appx"
+curl --location --progress-bar https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx --output %TEMP%\vclibs.appx
 powershell -c "Add-AppxPackage %TEMP%\vclibs.appx"
 curl --location --progress-bar https://github.com/hello-smile6/give-me-win32-email/releases/download/dependencies/microsoft.windowscommunicationsapps_16005.14326.20858.0_neutral_._8wekyb3d8bbwe.appxbundle --output "%TEMP%\microsoft.windowscommunicationsapps_16005.14326.20858.0_neutral_~_8wekyb3d8bbwe.appxbundle"
 powershell -c "Add-AppxPackage %TEMP%\microsoft.windowscommunicationsapps_16005.14326.20858.0_neutral_~_8wekyb3d8bbwe.appxbundle"
